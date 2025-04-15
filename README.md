3d548a03970af8b7c9fec0a633da293d9ac60126
Thien Luu <thien.luu@cas.de>
2025-04-15 13:08:50 +0200

    setup

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # url-shortener
+
+A custom version of Bit.ly. 
+
+Generate short links and track clicks, browsers, and geolocation.
+
+Including expiry logic and QR code generation.
+
+- **Tech Stack**: Spring Boot, Redis (for fast key lookup), Google Maps API
+- **Skills**: RESTful services, Redis, analytics, DevOps
---
+++ src/main/java/xyz/thnlu/url_shortener/HelloController.java
@@ -0,0 +1,13 @@
+package xyz.thnlu.url_shortener;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+}
